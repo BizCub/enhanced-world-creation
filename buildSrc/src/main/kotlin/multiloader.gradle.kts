008ee39b1c47b4fcb4276b846f1e7e280e0ec785
BizCub <bizcubmine@gmail.com>
2026-07-03 20:11:29 +0200

    Updated Gradle dependencies Added issue templates Fix publish versions

--- buildSrc/src/main/kotlin/multiloader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader.gradle.kts
@@ -1,6 +1,55 @@
 plugins {
     id("me.modmuss50.mod-publish-plugin")
+    id("dev.kikugie.fletching-table")
     id("com.bizcub.multiloader")
 }
 
-multiloader.init()
+multiloader {
+    val isClothConfigAvailable = !(isForge && scp > "1.21.3")
+
+    sc.constants["is_cloth_config_available"] = isClothConfigAvailable
+
+    sc.replacements {
+        string(scp >= "26.2") {
+            replace(".setScreen(", ".gui.setScreen(")
+        }
+        string(scp >= "26.1", "!graphics") {
+            replace("GuiGraphics", "GuiGraphicsExtractor")
+        }
+        string(scp >= "1.21.11" && !isForge, "auto_config") {
+            replace("AutoConfig", "AutoConfigClient")
+        }
+        string(scp >= "1.21.11") {
+            replace("ResourceLocation", "Identifier")
+        }
+        string(scp >= "1.19.4") {
+            replace("CreateWorldScreen.SelectedGameMode", "WorldCreationUiState.SelectedGameMode")
+        }
+    }
+
+    addDependency(
+        dependency = "me.shedaniel.cloth:cloth-config-${mod.loader}:${getDep("cloth-config").split("+").first()}",
+        configuration = if (isClothConfigAvailable) "implementation" else "compileOnly",
+        repository = "maven.shedaniel.me",
+        excludedModules = listOf("net.fabricmc.fabric-api"),
+        isPublishDepEnabled = isClothConfigAvailable,
+        publishProjectId = "cloth-config"
+    )
+
+    if (isFabric) {
+        addDependency(
+            dependency = "net.fabricmc:fabric-loader:${getDep("fabric")}"
+        )
+        addDependency(
+            dependency = "net.fabricmc.fabric-api:fabric-api:${getDep("fabric-api")}",
+            isPublishDepEnabled = true,
+            publishRequirement = "requires"
+        )
+        addDependency(
+            dependency = "com.terraformersmc:modmenu:${getDep("modmenu")}",
+            repository = "maven.terraformersmc.com/releases",
+            excludedModules = listOf("eu.pb4"),
+            isPublishDepEnabled = true
+        )
+    }
+}
---
+++ buildscripts/forge.arch.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    multiloader
+    alias(libs.plugins.loom.arch)
+}
+
+multiloader {
+    loom.silentMojangMappingsLicense()
+
+    java.toolchain.languageVersion.set(JavaLanguageVersion.of(mod.javaNumber))
+
+    repositories {
+        for (rep in reps) maven(rep.repository)
+    }
+
+    dependencies {
+        minecraft("com.mojang:minecraft:${mod.mc}")
+        mappings(loom.officialMojangMappings())
+        "forge"("net.minecraftforge:forge:${getDep("forge")}")
+        for (dep in deps) dep.configuration(dep.dependency) {
+            for (module in eModules) exclude(module.module)
+        }
+    }
+
+    loom {
+        runConfigs.getByName("client") { runDirectory.set(clientRunFile) }
+        runConfigs.getByName("server") { runDirectory.set(serverRunFile) }
+
+        forge.mixinConfigs("${mod.mixin}.mixins.json")
+        val awFile = file(ctForgeArchPath)
+        if (awFile.exists()) accessWidenerPath.set(awFile)
+
+        decompilers {
+            get("vineflower").apply {
+                options.put("mark-corresponding-synthetics", "1")
+            }
+        }
+    }
+
+    val builtFile = tasks.remapJar.get().archiveFile
+
+    publishMods {
+        file.set(builtFile)
+    }
+
+    tasks {
+        named<Copy>("buildAndCollect") {
+            from(builtFile)
+        }
+
+        remapJar {
+            injectAccessWidener = true
+        }
+
+        validateAccessWidener {
+            enabled = false
+        }
+
+        named("validateAccessWidener") {
+            dependsOn("processResources")
+        }
+    }
+}
